a6dd3ea7314c3aa1532bc1d490e47a96d6de1ba4
Alex Pushkin <ifc@suz-ppk.ru>
2024-10-07 15:05:23 +1000

    Добавьте файлы проекта.

---
+++ 1145_git/IDraw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1145_git
+{
+    public interface IDraw
+    {
+        void Draw(System.Drawing.Graphics graphics);
+    }
+}
---
+++ 1145_git/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+
+namespace _1145_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graphics graphics = Graphics.FromHwnd(Process.GetCurrentProcess().MainWindowHandle);
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+
+            foreach (var type in types)
+            {
+                if (type.IsPublic && type.IsClass)
+                { 
+                    var instance = Activator.CreateInstance(type) as IDraw;
+                    instance.Draw(graphics);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
