6f22019c7026d8017a90c6e6344c9feafcfda9ad
Student <Student@CAB4-10>
2024-10-07 15:19:03 +1000

    1 группа(караси)

---
+++ 1145_git/Funcs/Karasi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1145_git.Funcs
+{
+    public class Karasi : IDraw 
+    {
+        public void Draw(Graphics graphics)
+        {
+            Bitmap bitmap = new Bitmap("karasi.jpg");
+            graphics.DrawImage(bitmap, new Point(225, 1));
+        }
+    }
+}
