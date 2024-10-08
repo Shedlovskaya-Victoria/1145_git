856eb268de3ee55b81ccf9d3361a520206db27b7
Alex Pushkin <ifc@suz-ppk.ru>
2024-10-08 15:44:02 +1000

    with love

--- 1145_git/Funcs/Я.cs
+++ 1145_git/Funcs/Я.cs
@@ -12,7 +12,7 @@ namespace _1145_git.Funcs
         public void Draw(Graphics graphics)
         {
             for(int i = 0; i  < 10; i++) 
-                graphics.DrawString("THE END", new Font(FontFamily.GenericSerif, 200), new SolidBrush(Color.Red), new PointF(150, 200 * i));
+                graphics.DrawString("♥☺☻☺♥", new Font(FontFamily.GenericSerif, 200), new SolidBrush(Color.Red), new PointF(150, 200 * i));
         }
     }
 }
