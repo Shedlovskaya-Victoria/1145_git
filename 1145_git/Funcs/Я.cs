7a07008726f4335c23130bf9960bfe3deb8cb91f
Alex Pushkin <ifc@suz-ppk.ru>
2024-10-08 15:33:19 +1000

    end

---
+++ 1145_git/Funcs/Я.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1145_git.Funcs
+{
+    public class Я : IDraw
+    {
+        public void Draw(Graphics graphics)
+        {
+            for(int i = 0; i  < 10; i++) 
+            graphics.DrawString("THE END", new Font(FontFamily.GenericSerif, 200), new SolidBrush(Color.Red), new PointF(150, 150 * i));
+        }
+    }
+}
